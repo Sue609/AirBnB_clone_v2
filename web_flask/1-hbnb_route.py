701c9a8928b0541ceedfdeee431158d9ba8b8050
Sue609 <122805048+Sue609@users.noreply.github.com>
2023-09-21 17:40:34 +0300

    script that starts a Flask web application

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+
+def hello_hbnb():
+    """
+    Script that displays "hello HBNB".
+    """
+    return "Hello HBNB"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Script that displays HBNB
+    """
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=500)
