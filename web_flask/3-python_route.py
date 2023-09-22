9620ce1aa9cdf77c40c166511de458bef68cbe4a
Sue609 <susankamau609@gmail.com>
2023-09-22 03:36:29 -0700

    Script that displays python followed by text.'

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+This module introduces functions.
+"""
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
@@ -10,12 +14,14 @@ def hello_hbnb():
     """
     return "Hello HBNB"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """
     Script that displays hbnb
     """
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_route(text):
@@ -25,6 +31,7 @@ def c_route(text):
     text = text.replace("_", " ")
     return f"C {text}"
 
+
 @app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_route(text="is cool"):
@@ -32,6 +39,7 @@ def python_route(text="is cool"):
     Funtion that displays python followed by text
     """
     return "Python {}".format(text.replace('_', ' '))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
