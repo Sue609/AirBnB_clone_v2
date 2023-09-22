0f2119f6a4167adee60d96efa41a471edd065386
Sue609 <susankamau609@gmail.com>
2023-09-22 03:31:08 -0700

    Flask script that displays C followed by text.

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
+"""
+This module introduces a new function.
+"""
+
+
 from flask import Flask
- 
+
 app = Flask(__name__)
- 
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     Script that displays hello HBNB
     """
     return "Hello HBNB"
- 
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbhn():
     """
     Script that displays hbnb
     """
     return "hbnb"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
@@ -24,6 +32,7 @@ def c_route(text):
     """
     text = text.replace("_", " ")
     return f"C {text}"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
