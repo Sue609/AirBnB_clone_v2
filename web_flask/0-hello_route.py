de59c89080e13a535a78355fb85ad3dd251a1ea3
Sue609 <susankamau609@gmail.com>
2023-09-22 03:02:34 -0700

    Pycodestyle.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,13 +6,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     Script that displays “Hello HBNB!”
     """
-    return  "Hello HBNB!"
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
